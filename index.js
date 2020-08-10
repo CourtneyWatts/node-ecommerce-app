6218b94bea84f59067e563339cb467b340ad440a
CourtneyWatts <courtney.watts@icloudm>
2020-08-10 15:15:08 +0100

    added express + bodyParser

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hi there')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `)
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('account created')
+})
+app.listen(3000, () => {
+  console.log('listening')
+})
